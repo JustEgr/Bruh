28ae818587035136eb417d1e2f5cb3ea1f5399bd
JustEgr <we.are.number.one@yandex.ru>
2020-02-11 18:07:50 +0700

    Первое задание первой лабы 11 вариант

---
+++ laba1/laba1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream> 
+#include <fstream> 
+#include <string> 
+using namespace std;
+
+int main()
+{
+	int* mass;
+	int k;
+	cout << "Enter the size of massive:"; cin >> k;
+	mass = new int[k];
+	string path = "C:\\Work\\laba1\\Text1.txt";
+	ifstream fin;
+	fin.open(path);
+	if (!fin.is_open())
+	{
+		cout << "ERROR\n";
+		return 0;
+	}
+	for (int i = 0; i < k; i++)
+		fin >> mass[i];
+	for (int i = 0; i < k; i++)
+		cout << mass[i] << "\n";
+}
