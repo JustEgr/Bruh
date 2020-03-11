f894a30c677d51b5d22fe70c12770131c7d74c0c
JustEgr <we.are.number.one@yandex.ru>
2020-03-11 22:07:23 +0700

    нормальное первое задание первой лабы плюс 3-е задание второй

--- laba1/laba1.cpp
+++ laba1/laba1.cpp
@@ -5,10 +5,14 @@ using namespace std;
 
 int main()
 {
+	int size = 0, a;
+	ifstream input("Text1.txt");
+	while (input >> a)
+		size++;
 	int* mass;
 	int k;
-	cout << "Enter the size of massive:"; cin >> k;
-	mass = new int[k];
+	cout << "Enter k:"; cin >> k;
+	mass = new int[size];
 	string path = "C:\\Work\\laba1\\Text1.txt";
 	ifstream fin;
 	fin.open(path);
@@ -17,8 +21,20 @@ int main()
 		cout << "ERROR\n";
 		return 0;
 	}
-	for (int i = 0; i < k; i++)
-		fin >> mass[i];
-	for (int i = 0; i < k; i++)
-		cout << mass[i] << "\n";
+	for (int i = 0; i < size; i++)
+		if (mass[i] < k)
+			fin >> mass[i];
+	int temp;
+	for (int i = 0; i < size - 1; i++)
+		for (int j = 0; j < size - i - 1; j++)
+			if (mass[j] > mass[j + 1])
+			{
+				temp = mass[j];
+				mass[j] = mass[j + 1];
+				mass[j + 1] = temp;
+			}
+	for (int i = 0; i < size; i++)
+		if (mass[i] < k)
+			cout << mass[i] << "\n";
 }
+
