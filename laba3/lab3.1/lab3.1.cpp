d6b6555526f76004f4f16de34b4715de0b4c4994
JustEgr <we.are.number.one@yandex.ru>
2020-05-02 21:47:00 +0700

    Третья лаба - строки. Первое задание.

---
+++ laba3/lab3.1/lab3.1.cpp
@@ -0,0 +1,15 @@
+﻿#include <cstdio>
+
+int main()
+{
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+        if ('A' <= c && c <= 'Z')
+        {
+            c += 'a' - 'A';
+        }
+        putchar(c);
+    }
+    return 0;
+}
