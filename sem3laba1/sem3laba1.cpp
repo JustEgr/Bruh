1440157b9d1abfd4cd2ba506680febd8d8721031
JustEgr <we.are.number.one@yandex.ru>
2020-10-04 22:03:02 +0700

    3 семак структуры

---
+++ sem3laba1/sem3laba1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+struct complex
+{
+	double k, l;
+};
+
+void Comp_slozh(complex a, complex b)
+{
+	cout << "Summa :" << a.k + b.k << " " << a.l + b.l << endl;
+}
+void Comp_umnozh(complex a, complex b)
+{
+	cout << "Proizvedenie :" << a.k * b.k << " " << a.l * b.l;
+}
+
+int main()
+{
+	complex a, b;
+	cout << "Complex a: ";
+	cin >> a.k >> a.l;
+	cout << "Complex b: ";
+	cin >> b.k >> b.l;
+	Comp_slozh(a, b);
+	Comp_umnozh(a, b);
+}
